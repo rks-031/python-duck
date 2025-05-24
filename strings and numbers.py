833ef3c0c4da2bb5853b972ce2554a7851209f54
Rajnish Kumar Singh <rajnish31july@gmail.com>
2025-05-24 21:39:54 +0530

    covered: strings, numbers, variables, functions, scope, and user input

---
+++ strings and numbers.py
@@ -0,0 +1,11 @@
+# print("Hello World!")
+# print(type(2))
+# print(type(2.0))
+
+print(f"There are {20*24*60} minutes in {20} days")
+
+print("There are " + str(20*24*60) + " minutes in " + str(20) + " days")
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,9 @@
+days  = 20
+minutes = 20*24*60
+print(f"There are {minutes} minutes in {days} days")
+
+days = 20
+to_seconds = 20*24*60*60
+print(f"There are {to_seconds} seconds in {days} days!")
+
+# always use meaningful variable names
