71b88cf06b3846a3c823bf97f717b5bcb5cac3ae
Rajnish Kumar Singh <rajnish31july@gmail.com>
2025-05-25 21:06:06 +0530

    covered: basics of oops in py

---
+++ oops/post.py
@@ -0,0 +1,8 @@
+class Post:
+
+    def __init__(self, message, author):
+        self.message = message
+        self.author = author
+
+    def get_post_info(self):
+        print(f"Post by {self.author}: {self.message}")
