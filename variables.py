2a3f954fc1bfe78e936f9af37c45523f45ecf8a7
Rajnish Kumar Singh <rajnish31july@gmail.com>
2025-05-26 22:07:50 +0530

    Day 1 tasks completed

--- variables.py
+++
@@ -1,9 +0,0 @@
-days  = 20
-minutes = 20*24*60
-print(f"There are {minutes} minutes in {days} days")
-
-days = 20
-to_seconds = 20*24*60*60
-print(f"There are {to_seconds} seconds in {days} days!")
-
-# always use meaningful variable names
