b658d4ac2a16f55fdcf811b0144a0ccc21c302cf
Rajnish Kumar Singh <rajnish31july@gmail.com>
2025-06-09 20:19:01 +0530

    method for solving the find a string problem

--- Day-8/find a string.py
+++ Day-8/find a string.py
@@ -1,14 +1,25 @@
+# method 1
+# def count_substring(string, sub_string):
+#     len_of_substr = len(sub_string)
+#     first_ch = sub_string[0]
+#     count = 0
+#     for i in range(0, len(string) - len_of_substr + 1):
+#         if string[i] == first_ch:
+#             str = ""
+#             for j in range(i, i+len_of_substr):
+#                 str += string[j]
+#             if str == sub_string:
+#                 count += 1
+#     return count
+
+# method 2
 def count_substring(string, sub_string):
     len_of_substr = len(sub_string)
-    first_ch = sub_string[0]
     count = 0
     for i in range(0, len(string) - len_of_substr + 1):
-        if string[i] == first_ch:
-            str = ""
-            for j in range(i, i+len_of_substr):
-                str += string[j]
-            if str == sub_string:
-                count += 1
+        str = string[i:i+len_of_substr]
+        if str == sub_string:
+            count += 1
     return count
 
 if __name__ == '__main__':
